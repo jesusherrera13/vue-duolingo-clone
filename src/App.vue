4a0b65905feb137d5563f7555dff063b21dd19cf
Jesús Herrera <jesusherrera13@gmail.com>
2023-02-21 11:19:13 -0700

    drawer x2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,55 @@
 <template>
-  <MenuTop />
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer>
+        <v-list>
+          <v-list-item title="Navigation drawer"></v-list-item>
+        </v-list>
+        <v-divider></v-divider>
+
+          <v-list-item
+            v-for="(option, i) in menu_options"
+            :key="i"
+            :value="option"
+            active-color="primary"
+            :to="option.path"
+          >
+            <template v-slot:prepend>
+              <v-icon 
+                :icon="option.icon"
+                :color="option.color"
+              ></v-icon>
+            </template>
+
+            <v-list-item-title v-text="option.text"></v-list-item-title>
+          </v-list-item>
+      </v-navigation-drawer>
+
+
+      <v-main style="min-height: 300px;"></v-main>
+
+      <v-navigation-drawer location="right">
+        <v-list>
+          <v-list-item title="Navigation drawer"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
-import MenuTop from './components/MenuTop.vue'
 export default {
+  data() {
+    return {
+      menu_options: [
+        { text: 'Learn', icon: 'mdi-home', color: 'teal',path: '/learn' },
+        { text: 'Practice', icon: 'mdi-weight-lifter', color: 'red', path: '/practice-hub' },
+        { text: 'Store', icon: 'mdi-storefront', color: 'purple', path: '/shop' },
+        { text: 'Profile', icon: 'mdi-account', color: 'blue', path: '/account' },
+      ]
+    }
+  },
   components: {
-    MenuTop
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import naive from 'naive-ui/es/preset'
 
-createApp(App).use(naive).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css';
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    /* theme: {
+        defaultTheme: 'dark'
+    } */
+});
+
+const app = createApp(App);
+app.use(vuetify);
+
+
+app.mount('#app')
